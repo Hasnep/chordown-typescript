b7e42a54ffc4c95b64d1d7d85487e3b843be4e9b
Hannes <hasnep@gmail.com>
2019-06-05 23:35:23 +0100

    Add header to onsong export

--- src/exporters/export-onsong.ts
+++ src/exporters/export-onsong.ts
@@ -3,6 +3,24 @@ import { Chordown } from "../chordown";
 export function export_onsong(chordown: Chordown): string {
   let out: string = "";
 
+  out += chordown.header.title;
+  if (chordown.header.hasOwnProperty("subtitle")) {
+    out += " (" + chordown.header.subtitle + ")";
+  }
+  out += "\n";
+  if (chordown.header.hasOwnProperty("artist")) {
+    out += chordown.header.artist;
+  }
+  out += "\n";
+  out += "Key:";
+  if (chordown.header.hasOwnProperty("key")) {
+    out += " [" + chordown.header.key + "]";
+  }
+  out += "\n";
+  if (chordown.header.hasOwnProperty("capo")) {
+    out += "Capo: " + chordown.header.key + "\n";
+  }
+
   //   for (let header_key of Object.keys(chordown.header)) {
   //     out += `${to_sentence_case(header_key)}: ${
   //       chordown.header[header_key]
