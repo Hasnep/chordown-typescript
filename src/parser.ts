ceaf77532784810e54e5ff739c8425286643229c
Hannes <hasnep@gmail.com>
2019-08-27 22:49:24 +0100

    Make remembered chords FIFO instead of LIFO

--- src/parser.ts
+++ src/parser.ts
@@ -153,7 +153,8 @@ export function parse_body(body: string): ISection[] {
         // if there are no chords currently, try to remember chords
         if (current_line.chords == null) {
           for (let i = 0; i < current_line.lyrics.length - 1; i++) {
-            current_line.chords.push(remembered_chords.pop());
+            console.log(remembered_chords)
+            current_line.chords.push(remembered_chords.shift());
           }
         }
 
