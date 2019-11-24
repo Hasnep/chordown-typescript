3cebc7d558236ba5c2e53b6eb9249f36c23eeade
Hannes <hasnep@gmail.com>
2019-11-24 21:59:36 +0000

    Add bass notes to transpose tests

--- tests/test-transpose.ts
+++ tests/test-transpose.ts
@@ -20,6 +20,15 @@ describe("separate_chord", function() {
     };
     assert.deepEqual(separate_chord("Bbmaj7/9"), expected_output);
   });
+
+  it("separates a complex chord with a bass note", function() {
+    const expected_output: IChord = {
+      root: "Bb",
+      type: "maj7/9",
+      bass: "Gb",
+    };
+    assert.deepEqual(separate_chord("Bbmaj7/9/Gb"), expected_output);
+  });
 });
 
 describe("transpose_chord", function() {
@@ -47,6 +56,20 @@ describe("transpose_chord", function() {
       root: "G#",
       type: "maj7/9",
       bass: undefined,
+    };
+    assert.deepEqual(transpose_chord(chord_input, -2), expected_output);
+  });
+
+  it("transposes a complex chord with a bass note", function() {
+    const chord_input: IChord = {
+      root: "Bb",
+      type: "maj7/9",
+      bass: "Gb",
+    };
+    const expected_output: IChord = {
+      root: "G#",
+      type: "maj7/9",
+      bass: "E",
     };
     assert.deepEqual(transpose_chord(chord_input, -2), expected_output);
   });
